1a02f4e3175719d7cd8cd2a2172dafb67cc9f2af
Daniel Imhoff <dwieeb@gmail.com>
2020-02-11 10:23:45 -0800

    update JS deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'dist/esm/index.js',
   output: {
+    banner: '/*! Ionic Couchbase Lite 2.x Integration : https://ionicframework.com/ - Commercially Licensed */',
     file: 'dist/ionic-enterprise-couchbase-lite.js',
-    format: 'cjs'
+    format: 'cjs',
+    sourcemap: true,
   },
-  sourcemap: true,
-  banner: '/*! Ionic Couchbase Lite 2.x Integration : https://ionicframework.com/ - Commercially Licensed */',
   plugins: [
     nodeResolve()
   ]
