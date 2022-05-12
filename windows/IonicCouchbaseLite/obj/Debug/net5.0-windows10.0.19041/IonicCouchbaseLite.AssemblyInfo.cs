b054b7df97eadf760846d4bbcc43bb80d69c7f9c
Max Lynch <max@ionic.io>
2022-05-12 10:35:01 -0500

    1.5.0 nupkg

--- windows/IonicCouchbaseLite/obj/Debug/net5.0-windows10.0.19041/IonicCouchbaseLite.AssemblyInfo.cs
+++ windows/IonicCouchbaseLite/obj/Debug/net5.0-windows10.0.19041/IonicCouchbaseLite.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("IonicCouchbaseLite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.5.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IonicCouchbaseLite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IonicCouchbaseLite")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.5.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows10.0.19041.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows10.0.19041.0")]
 
